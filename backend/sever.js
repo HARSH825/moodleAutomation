3ca3433d59595743c9a91f0988a0fa242db35f4e
HARSH825 <harshchhallani3@gmail.com>
2025-04-17 10:27:51 +0530

    structured backend , cleaned endpoints from here

--- backend/sever.js
+++ backend/sever.js
@@ -2,10 +2,13 @@ import express from 'express';
 import loginFetchRouter from './route/loginFetch.js';
 import checkSubRouter from './route/checkSub.js';
 import generateDocumentsRouter from './route/genDocRoute.js';
+import cors from 'cors';
+
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use('/loginFetch',loginFetchRouter);
 app.use('/checkSubmissions',checkSubRouter);
 app.use('/generateDocuments',generateDocumentsRouter);
